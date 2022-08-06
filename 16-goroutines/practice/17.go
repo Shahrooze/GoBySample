52f6c6dd5358e9c0023bdbc2386f8e8bdd6f2e41
shahrooz <shahrooz.jefri@gmail.com>
2022-08-06 13:35:31 +0430

    adding chapters

---
+++ 16-goroutines/practice/17.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func worker(id int, jobs <-chan int, results chan<- int) {
+	for {
+		fmt.Println("Waiting", id)
+		j, open := <-jobs
+		if !open {
+			break
+		}
+		fmt.Println("worker", id, "started  job", j)
+		results <- j * 2
+		fmt.Println("worker", id, "finished job", j)
+
+	}
+}
+
+func main() {
+
+	const numJobs = 10000
+	jobs := make(chan int, numJobs)
+	results := make(chan int, numJobs)
+
+	for w := 1; w <= 3; w++ {
+
+		go worker(w, jobs, results)
+	}
+
+	for j := 1; j <= numJobs; j++ {
+		time.Sleep(time.Second * 10)
+		jobs <- j
+	}
+	close(jobs)
+
+	for a := 1; a <= numJobs; a++ {
+		fmt.Println(<-results)
+	}
+}
